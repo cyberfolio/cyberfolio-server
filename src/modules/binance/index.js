53b90569e95319463c004dd24c2d450fcd4a85bc
onuriltan <onur.iltan@gmail.com>
2021-10-10 01:49:16 +0300

    refactor: move logic to services and add endpoint for fiat deposit&withdraws

--- src/modules/binance/index.js
+++ src/modules/binance/index.js
@@ -1,72 +1,71 @@
 const express = require("express");
-const crypto = require("crypto-js");
 const router = express.Router();
-const axios = require("axios");
 
-const API_KEY = process.env.BINANCE_API_KEY;
-const API_SECRET = process.env.BINANCE_API_SECRET;
-const API_URL = process.env.BINANCE_API_URL;
-
-
-const getHoldings = async (type) => {
-  const queryString = `type=${type}&timestamp=${Date.now()}`;
-  const signature = crypto
-    .HmacSHA256(queryString, API_SECRET)
-    .toString(crypto.enc.Hex);
-  try {
-    const response = await axios({
-      url: `${API_URL}/sapi/v1/accountSnapshot?${queryString}&signature=${signature}`,
-      method: "get",
-      headers: {
-        "X-MBX-APIKEY": API_KEY,
-      },
-    });
-
-    let data = response.data;
-    let latestSnapshotIndex = 0;
-    if (data?.snapshotVos?.length) {
-      latestSnapshotIndex = data.snapshotVos.length - 1
-    }
-    console.log(data)
-    if (data?.snapshotVos[latestSnapshotIndex]?.data?.balances[0]?.free) {
-      const assetsBiggerThanZero = data?.snapshotVos[latestSnapshotIndex]?.data?.balances?.filter(asset =>
-        asset.free > 0
-      );
-      data = assetsBiggerThanZero;
-    }
-    return data
-  } catch (e) {
-    if(e?.response?.data?.code) {
-      throw new Error(e.response.data.code)
-    } else {
-      throw new Error(e.message)
-    }
-  }
-}
+const {
+  getHoldings,
+  getFiatDepositAndWithDrawalHistory,
+  getFiatPaymentBuyAndSellHistory,
+} = require("./services");
 
 router.get("/spot", async (req, res, next) => {
   try {
-    const data = await getHoldings('SPOT')
+    const data = await getHoldings("SPOT");
     res.json(data);
-  }catch (e){
+  } catch (e) {
     next(e);
   }
 });
 
 router.get("/margin", async (req, res, next) => {
   try {
-    const data = await getHoldings('MARGIN')
+    const data = await getHoldings("MARGIN");
     res.json(data);
-  }catch (e){
+  } catch (e) {
     next(e);
   }
 });
 
 router.get("/futures", async (req, res, next) => {
   try {
-    const data = await getHoldings('FUTURES')
+    const data = await getHoldings("FUTURES");
     res.json(data);
-  }catch (e){
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/fiat-deposit-history", async (req, res, next) => {
+  try {
+    const data = await getFiatDepositAndWithDrawalHistory(0);
+    res.json(data);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/fiat-withdraw-history", async (req, res, next) => {
+  try {
+    const data = await getFiatDepositAndWithDrawalHistory(1);
+    res.json(data);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/fiat-payment-buy-history", async (req, res, next) => {
+  try {
+    const data = await getFiatPaymentBuyAndSellHistory(1);
+    res.json(data);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/fiat-payment-sell-history", async (req, res, next) => {
+  try {
+    const data = await getFiatPaymentBuyAndSellHistory(1);
+    res.json(data);
+  } catch (e) {
     next(e);
   }
 });
---
+++ src/modules/binance/services.js
@@ -0,0 +1,97 @@
+const axios = require("axios");
+const crypto = require("crypto-js");
+
+const API_KEY = process.env.BINANCE_API_KEY;
+const API_SECRET = process.env.BINANCE_API_SECRET;
+const API_URL = process.env.BINANCE_API_URL;
+
+const getHoldings = async (type) => {
+  const queryString = `type=${type}&timestamp=${Date.now()}`;
+  const signature = crypto
+    .HmacSHA256(queryString, API_SECRET)
+    .toString(crypto.enc.Hex);
+  try {
+    const response = await axios({
+      url: `${API_URL}/sapi/v1/accountSnapshot?${queryString}&signature=${signature}`,
+      method: "get",
+      headers: {
+        "X-MBX-APIKEY": API_KEY,
+      },
+    });
+
+    let data = response.data;
+    let latestSnapshotIndex = 0;
+    if (data?.snapshotVos?.length) {
+      latestSnapshotIndex = data.snapshotVos.length - 1;
+    }
+    if (data?.snapshotVos[latestSnapshotIndex]?.data?.balances[0]?.free) {
+      const assetsBiggerThanZero = data?.snapshotVos[
+        latestSnapshotIndex
+      ]?.data?.balances?.filter((asset) => asset.free > 0);
+      data = assetsBiggerThanZero;
+    }
+    return data;
+  } catch (e) {
+    if (e?.response?.data?.code) {
+      throw new Error(e.response.data.code);
+    } else {
+      throw new Error(e.message);
+    }
+  }
+};
+
+const getFiatDepositAndWithDrawalHistory = async (transactionType) => {
+  const queryString = `transactionType=${transactionType}&timestamp=${Date.now()}`;
+  const signature = crypto
+    .HmacSHA256(queryString, API_SECRET)
+    .toString(crypto.enc.Hex);
+  try {
+    const response = await axios({
+      url: `${API_URL}/sapi/v1/fiat/orders?${queryString}&signature=${signature}`,
+      method: "get",
+      headers: {
+        "X-MBX-APIKEY": API_KEY,
+      },
+    });
+
+    let data = response.data;
+    return data;
+  } catch (e) {
+    if (e?.response?.data?.code) {
+      throw new Error(e.response.data.code);
+    } else {
+      throw new Error(e.message);
+    }
+  }
+};
+
+const getFiatPaymentBuyAndSellHistory = async (transactionType) => {
+    const queryString = `transactionType=${transactionType}&timestamp=${Date.now()}`;
+    const signature = crypto
+      .HmacSHA256(queryString, API_SECRET)
+      .toString(crypto.enc.Hex);
+    try {
+      const response = await axios({
+        url: `${API_URL}/sapi/v1/fiat/payments?${queryString}&signature=${signature}`,
+        method: "get",
+        headers: {
+          "X-MBX-APIKEY": API_KEY,
+        },
+      });
+  
+      let data = response.data;
+      return data;
+    } catch (e) {
+      if (e?.response?.data?.code) {
+        throw new Error(e.response.data.code);
+      } else {
+        throw new Error(e.message);
+      }
+    }
+  };
+
+module.exports = {
+  getHoldings,
+  getFiatDepositAndWithDrawalHistory,
+  getFiatPaymentBuyAndSellHistory
+};
