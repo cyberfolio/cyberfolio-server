3fdd4d8a8422bc5db7da765a10dd37ce226e50c4
onuriltan <onur.iltan@gmail.com>
2022-05-26 10:29:20 +0300

    fix: logs

--- src/modules/cron/index.ts
+++ src/modules/cron/index.ts
@@ -5,7 +5,9 @@ const everyHourCronValue = '0 0 */1 * * *'
 
 export const initCronJobs = async () => {
   try {
+    console.log('Cryptoprice update started at: ' + new Date())
     await updateCurrencies()
+    console.log('Cryptoprice update completed at: ' + new Date())
   } catch (e) {
     console.log('Cryptoprice update failed at, reason: ' + e.message)
   }
