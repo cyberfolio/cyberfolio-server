0dfc3100383d84c845c31a7ec4a60f2a44b90664
onuriltan <onur.iltan@gmail.com>
2022-05-26 08:59:24 +0300

    fix: price update

--- src/modules/cron/updateCurrencies.ts
+++ src/modules/cron/updateCurrencies.ts
@@ -1,32 +1,13 @@
-// import { sleep } from "@src/utils";
-// import {
-//    getLastCurrencyUpdate,
-//    addOrUpdateAllCryptoPriceInUSD,
-// } from '@providers/coingecko'
+import { sleep } from '@src/utils'
+import { addOrUpdateAllCryptoPriceInUSD } from '@providers/coingecko'
 
 export const updateCurrencies = async () => {
-  // const lastUpdate = await getLastCurrencyUpdate();
-  // if (lastUpdate) {
-  //   const diff = new Date().valueOf() - lastUpdate.valueOf();
-  //   const hourDifference = diff / 1000 / 60 / 60;
-  //   if (hourDifference >= 1) {
-  //     for (let i = 1; i <= 6000; i++) {
-  //       try {
-  //         await addOrUpdateAllCryptoPriceInUSD(i);
-  //         await sleep(1000);
-  //       } catch (e) {
-  //         throw new Error(e);
-  //       }
-  //     }
-  //   }
-  // } else {
-  //   for (let i = 1; i <= 6000; i++) {
-  //     try {
-  //       await addOrUpdateAllCryptoPriceInUSD(i);
-  //       await sleep(1000);
-  //     } catch (e) {
-  //       throw new Error(e);
-  //     }
-  //   }
-  // }
+  for (let i = 1; i <= 6000; i++) {
+    try {
+      await addOrUpdateAllCryptoPriceInUSD(i)
+      await sleep(2000)
+    } catch (e) {
+      throw new Error(e)
+    }
+  }
 }
