6cc7497610736f1b6c18e305640705e5b41906a7
onuriltan <onur.iltan@gmail.com>
2022-05-29 12:18:03 +0300

    feat: ens name endpoint

--- src/api/info/index.ts
+++ src/api/info/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 const router = express.Router()
 
 import { getNetWorth, getAvailableAccounts } from './services'
+import { ethers } from 'ethers'
 
 router.get('/networth', async (req: any, res: express.Response) => {
   const keyIdentifier = req.keyIdentifier
@@ -20,6 +21,23 @@ router.get('/available-accounts', async (req: any, res: express.Response) => {
     return res.status(200).send({ availableAccounts })
   } catch (e) {
     return res.status(500).send(e.message)
+  }
+})
+
+router.get('/ens-name', async (req: any, res: express.Response) => {
+  const keyIdentifier = req.keyIdentifier
+  try {
+    const provider = new ethers.providers.JsonRpcProvider(
+      `${process.env.INFURA_API_URL}/${process.env.INFURA_PROJECT_ID}`,
+    )
+    const ensName = await provider.lookupAddress(keyIdentifier)
+    if (ensName) {
+      return res.status(200).send({ ensName })
+    } else {
+      return res.status(200).send('')
+    }
+  } catch (e) {
+    return res.status(401).send(e.message)
   }
 })
 
