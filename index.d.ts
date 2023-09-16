37fc98a3dcf8ecfd217280ab59e89e190456322e
Onur Iltan <7993238+onuriltan@users.noreply.github.com>
2023-09-16 14:27:52 +0300

    refactor: app for prod

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      MONGO_URL: string;
+      PORT: string;
+      FRONTEND_URL: string;
+      JWT_SECRET: string;
+      JWT_EXPIRY_IN_DAYS: string;
+      NODE_ENV: string;
+
+      INFURA_API_URL: string;
+      INFURA_PROJECT_ID: string;
+
+      COINGECKO_V3_API_URL: string;
+
+      COINMARKETCAP_API_KEY: string;
+      COINMARKETCAP_API_URL: string;
+
+      COVALENT_V1_API_URL: string;
+      COVALENT_API_KEY: string;
+
+      BLOCKCHAIN_INFO_API_URL: string;
+
+      SOLANA_MAINNET_CHAIN_ID: string;
+
+      BINANCE_API_URL: string;
+
+      KUCOIN_API_VERSION: string;
+      KUCOIN_API_URL: string;
+
+      GATEIO_API_HOST: string;
+      GATEIO_API_PREFIX: string;
+    }
+  }
+}
