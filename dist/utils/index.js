5408555c5eef75c18e3edec9287dc147533dcd9d
Onur Iltan <7993238+onuriltan@users.noreply.github.com>
2025-01-09 14:57:15 +0300

    refactor: read modules from @

---
+++ dist/utils/index.js
@@ -0,0 +1,149 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable no-promise-executor-return */
+/* eslint-disable promise/avoid-new */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-underscore-dangle */
+const web3_1 = __importDefault(require("web3"));
+const ethers_1 = require("ethers");
+const uuid_1 = require("uuid");
+const axios_1 = __importDefault(require("axios"));
+const logger_1 = __importDefault(require("@config/logger"));
+const lodash_1 = __importDefault(require("lodash"));
+const types_1 = require("@config/types");
+const model_1 = __importDefault(require("@modules/cron/scam-tokens/model"));
+const web3 = new web3_1.default(new web3_1.default.providers.HttpProvider(`${process.env.INFURA_API_URL}/${process.env.INFURA_PROJECT_ID}`));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const isValid0xAddress = (address) => web3.utils.isAddress(address);
+const formatBalance = (balance, decimals) => ethers_1.ethers.formatUnits(balance, Number(decimals));
+const sathoshiToBtcBalance = (satoshi) => satoshi * 0.00000001;
+const toBase64 = (string) => Buffer.from(string).toString('base64');
+const getScanUrl = (address, chain) => {
+    switch (chain) {
+        case types_1.Chain.ARBITRUM:
+            return `${types_1.ScanURL.ARBITRUM}/address/${address}`;
+        case types_1.Chain.AVALANCHE:
+            return `${types_1.ScanURL.AVALANCHE}/address/${address}`;
+        case types_1.Chain.BITCOIN:
+            return `${types_1.ScanURL.BITCOIN}/btc/address/${address}`;
+        case types_1.Chain.BSC:
+            return `${types_1.ScanURL.BSC}/address/${address}`;
+        case types_1.Chain.ETHEREUM:
+            return `${types_1.ScanURL.ETHEREUM}/address/${address}`;
+        case types_1.Chain.OPTIMISM:
+            return `${types_1.ScanURL.OPTIMISM}/address/${address}`;
+        case types_1.Chain.POLYGON:
+            return `${types_1.ScanURL.POLYGON}/address/${address}`;
+        case types_1.Chain.SOLANA:
+            return `${types_1.ScanURL.SOLANA}address/${address}`;
+        default:
+            return '';
+    }
+};
+const intDivide = (numerator, denominator) => Number((numerator / denominator).toString().split('.')[0]);
+const generateNonce = () => `I confirm that I'm the owner of this wallet by signing this message: ${(0, uuid_1.v4)()}`;
+const doesImageExists = async (url) => {
+    try {
+        await (0, axios_1.default)({
+            url,
+            method: 'get',
+        });
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
+};
+const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+const roundNumber = (num) => Math.round((num + Number.EPSILON) * 100) / 100;
+const onError = (e) => {
+    let error;
+    if (e instanceof Error) {
+        error = e;
+    }
+    else {
+        logger_1.default.error('Unexpected error', e);
+        error = new Error('Unexpected error');
+    }
+    return error;
+};
+const isEVMChain = (chain) => {
+    return (chain === types_1.Chain.ARBITRUM ||
+        chain === types_1.Chain.AVALANCHE ||
+        chain === types_1.Chain.BSC ||
+        chain === types_1.Chain.OPTIMISM ||
+        chain === types_1.Chain.POLYGON ||
+        chain === types_1.Chain.ETHEREUM);
+};
+const logError = ({ path, func, e }) => {
+    let message;
+    if (axios_1.default.isAxiosError(e)) {
+        message = e.response?.data ? JSON.stringify(e.response?.data, null, 2) : e.response?.statusText;
+    }
+    else if (e instanceof Error) {
+        message = e.message ? e.message : String(e);
+    }
+    else {
+        message = e;
+    }
+    logger_1.default.error(`Error at ${path} ${func} message: ${message}`);
+};
+const getFilePath = (path) => {
+    const fileName = path.substring(path.indexOf('src'));
+    return fileName;
+};
+const removeMongoFields = (object) => {
+    if (Object.prototype.toString.call(object) !== '[object Object]')
+        return object;
+    const obj = lodash_1.default.cloneDeep(object);
+    delete obj._id;
+    delete obj.__v;
+    return obj;
+};
+const isScamToken = async (address, chainId) => {
+    const isScam = await model_1.default.findOne({ address, chainId });
+    return Boolean(isScam);
+};
+function isEnumOf(object, possibleValue) {
+    return Object.values(object).includes(possibleValue);
+}
+const timestampToReadableDate = (timestamp) => {
+    const date = new Date(timestamp);
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    month = (month < 10 ? '0' : '') + month;
+    day = (day < 10 ? '0' : '') + day;
+    hour = (hour < 10 ? '0' : '') + hour;
+    min = (min < 10 ? '0' : '') + min;
+    sec = (sec < 10 ? '0' : '') + sec;
+    const str = `${day}/${month}/${date.getFullYear()} ${hour}:${min}:${sec}`;
+    return str;
+};
+const AppUtils = {
+    sleep,
+    isValid0xAddress,
+    formatBalance,
+    sathoshiToBtcBalance,
+    toBase64,
+    getScanUrl,
+    intDivide,
+    generateNonce,
+    doesImageExists,
+    capitalizeFirstLetter,
+    roundNumber,
+    onError,
+    isEVMChain,
+    logError,
+    getFilePath,
+    removeMongoFields,
+    isScamToken,
+    isEnumOf,
+    timestampToReadableDate,
+};
+exports.default = AppUtils;
---
+++ src/utils/index.ts
@@ -0,0 +1,169 @@
+/* eslint-disable no-promise-executor-return */
+/* eslint-disable promise/avoid-new */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-underscore-dangle */
+import Web3 from 'web3';
+import { ethers } from 'ethers';
+import { v4 as uuidv4 } from 'uuid';
+import axios, { AxiosError } from 'axios';
+import logger from '@config/logger';
+import _ from 'lodash';
+import { Chain, ScanURL } from '@config/types';
+import scamTokenModel from '@modules/cron/scam-tokens/model';
+
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(`${process.env.INFURA_API_URL}/${process.env.INFURA_PROJECT_ID}`),
+);
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const isValid0xAddress = (address: string) => web3.utils.isAddress(address);
+
+const formatBalance = (balance: string, decimals: string) => ethers.formatUnits(balance, Number(decimals));
+
+const sathoshiToBtcBalance = (satoshi: number) => satoshi * 0.00000001;
+
+const toBase64 = (string: string) => Buffer.from(string).toString('base64');
+
+const getScanUrl = (address: string, chain: Chain) => {
+  switch (chain) {
+    case Chain.ARBITRUM:
+      return `${ScanURL.ARBITRUM}/address/${address}`;
+    case Chain.AVALANCHE:
+      return `${ScanURL.AVALANCHE}/address/${address}`;
+    case Chain.BITCOIN:
+      return `${ScanURL.BITCOIN}/btc/address/${address}`;
+    case Chain.BSC:
+      return `${ScanURL.BSC}/address/${address}`;
+    case Chain.ETHEREUM:
+      return `${ScanURL.ETHEREUM}/address/${address}`;
+    case Chain.OPTIMISM:
+      return `${ScanURL.OPTIMISM}/address/${address}`;
+    case Chain.POLYGON:
+      return `${ScanURL.POLYGON}/address/${address}`;
+    case Chain.SOLANA:
+      return `${ScanURL.SOLANA}address/${address}`;
+    default:
+      return '';
+  }
+};
+
+const intDivide = (numerator: number, denominator: number) =>
+  Number((numerator / denominator).toString().split('.')[0]);
+
+const generateNonce = () => `I confirm that I'm the owner of this wallet by signing this message: ${uuidv4()}`;
+
+const doesImageExists = async (url: string) => {
+  try {
+    await axios({
+      url,
+      method: 'get',
+    });
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const capitalizeFirstLetter = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+const roundNumber = (num: number) => Math.round((num + Number.EPSILON) * 100) / 100;
+
+const onError = (e: unknown) => {
+  let error;
+  if (e instanceof Error) {
+    error = e;
+  } else {
+    logger.error('Unexpected error', e);
+    error = new Error('Unexpected error');
+  }
+  return error;
+};
+
+const isEVMChain = (chain: Chain) => {
+  return (
+    chain === Chain.ARBITRUM ||
+    chain === Chain.AVALANCHE ||
+    chain === Chain.BSC ||
+    chain === Chain.OPTIMISM ||
+    chain === Chain.POLYGON ||
+    chain === Chain.ETHEREUM
+  );
+};
+
+const logError = ({ path, func, e }: { path: string; func: string; e: Error | unknown | AxiosError<never> }) => {
+  let message;
+  if (axios.isAxiosError(e)) {
+    message = e.response?.data ? JSON.stringify(e.response?.data, null, 2) : e.response?.statusText;
+  } else if (e instanceof Error) {
+    message = e.message ? e.message : String(e);
+  } else {
+    message = e;
+  }
+  logger.error(`Error at ${path} ${func} message: ${message}`);
+};
+
+const getFilePath = (path: string) => {
+  const fileName = path.substring(path.indexOf('src'));
+  return fileName;
+};
+
+const removeMongoFields = (object: any) => {
+  if (Object.prototype.toString.call(object) !== '[object Object]') return object;
+  const obj = _.cloneDeep(object);
+  delete obj._id;
+  delete obj.__v;
+  return obj;
+};
+
+const isScamToken = async (address: string, chainId: string) => {
+  const isScam = await scamTokenModel.findOne({ address, chainId });
+  return Boolean(isScam);
+};
+
+function isEnumOf<T>(object: any, possibleValue: any): possibleValue is T[keyof T] {
+  return Object.values(object).includes(possibleValue);
+}
+
+const timestampToReadableDate = (timestamp: number) => {
+  const date = new Date(timestamp);
+  let month: number | string = date.getMonth() + 1;
+  let day: number | string = date.getDate();
+  let hour: number | string = date.getHours();
+  let min: number | string = date.getMinutes();
+  let sec: number | string = date.getSeconds();
+
+  month = (month < 10 ? '0' : '') + month;
+  day = (day < 10 ? '0' : '') + day;
+  hour = (hour < 10 ? '0' : '') + hour;
+  min = (min < 10 ? '0' : '') + min;
+  sec = (sec < 10 ? '0' : '') + sec;
+
+  const str = `${day}/${month}/${date.getFullYear()} ${hour}:${min}:${sec}`;
+
+  return str;
+};
+
+const AppUtils = {
+  sleep,
+  isValid0xAddress,
+  formatBalance,
+  sathoshiToBtcBalance,
+  toBase64,
+  getScanUrl,
+  intDivide,
+  generateNonce,
+  doesImageExists,
+  capitalizeFirstLetter,
+  roundNumber,
+  onError,
+  isEVMChain,
+  logError,
+  getFilePath,
+  removeMongoFields,
+  isScamToken,
+  isEnumOf,
+  timestampToReadableDate,
+};
+
+export default AppUtils;
