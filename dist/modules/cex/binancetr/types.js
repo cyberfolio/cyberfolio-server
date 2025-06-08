777a33c578db7c2a4571c9bbde110478fce0c9ae
Onur Iltan <7993238+onuriltan@users.noreply.github.com>
2025-06-08 21:30:24 +0300

    feat: remove dist from repo and install module alias

--- dist/modules/cex/binancetr/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var AccountType;
-(function (AccountType) {
-    AccountType["SPOT"] = "SPOT";
-    AccountType["MARGIN"] = "MARGIN";
-})(AccountType || (AccountType = {}));
